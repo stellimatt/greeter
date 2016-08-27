17adf263a969e7d8dddba2c62249bf04377c332d
stellimatt <matt.adams@stelligent.com>
2016-08-27 11:35:25 -0600

    remove unneeded block

--- pipelines/cookbooks/greeter/recipes/default.rb
+++ pipelines/cookbooks/greeter/recipes/default.rb
@@ -4,10 +4,6 @@ include_recipe "php::module_mysql"
 include_recipe "apache2::mod_php5"
 include_recipe "apache2"
 
-apache_conf "default" do
-  enable false
-end
-
 apache_site "default" do
   enable true
 end
